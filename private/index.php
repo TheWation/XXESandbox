370bca682ad90ad204e5b334cbed61ae38208abf
Wation <Github@Wation.net>
2023-06-08 02:10:57 +0330

    api key added

--- private/index.php
+++ private/index.php
@@ -1,12 +1,1 @@
-<?php
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        echo("Username or Password is wrong !");
-    }
-?>
-<h1>Admin Panel</h1>
-<form method="POST">
-        <label for="username">Username:</label>
-        <input id="username" type="text" placeholder="Username" name="username"><br>
-        Password: <input type="password" id="password" placeholder="Password" name="password"><br>
-        <input type="submit" value="Send">
-</form>
+apikey=c517e88a5ffc45ea996974808c11d3ba
